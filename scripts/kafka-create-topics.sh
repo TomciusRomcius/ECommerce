861ae8a718c7408ad740ca9aebfbd7b304a7b81b
Tomas Rimkevicius <tomas.ew88@gmail.com>
2026-06-25 14:22:12 +0300

    Create remaining topics

--- scripts/kafka-create-topics.sh
+++ scripts/kafka-create-topics.sh
@@ -7,136 +7,40 @@ done
 
 echo "Creating kafka topics"
 
-# Create kafka topics
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic charge-succeeded \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
+topics=(
+    charge_succeeded
+    product_created
+    product_updated
+    product_deleted
+    product_added_to_cart
+    product_cart_quantity_modified
+    product_removed_from_cart
+    user_cart_cleared
+    product_added_to_store
+    product_stock_updated
+    product_removed_from_store
+    store_created
+    store_updated
+    store_deleted
+    manufacturer_created
+    manufacturer_updated
+    manufacturer_deleted
+    category_created
+    category_updated
+    category_deleted
+    product_image_created
+    product_image_updated
+    product_image_deleted
+    payment_failed
+    user_account_deleted
+)
 
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic product-added-to-store \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic product-stock-updated \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic product-removed-from-store \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic product-created \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic product-updated \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic product-deleted \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic store-created \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic store-updated \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic store-deleted \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic manufacturer-created \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic manufacturer-updated \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic manufacturer-deleted \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic category-created \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic category-updated \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic category-deleted \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic product-image-created \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic product-image-updated \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
-
-kafka-topics.sh \
-    --bootstrap-server kafka:9092 \
-    --topic product-image-deleted \
-    --partitions 6 \
-    --replication-factor 1 \
-    --create > /dev/null 2>&1 || true
+for topic in "${topics[@]}"; do
+    kafka-topics.sh \
+        --bootstrap-server kafka:9092 \
+        --topic "$topic" \
+        --partitions 6 \
+        --replication-factor 1 \
+        --create > /dev/null 2>&1 || true
+    echo "Created topic: $topic"
+done
